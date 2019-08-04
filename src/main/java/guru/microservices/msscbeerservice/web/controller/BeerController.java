73fa5ec95f8bf33ff5c4d2c058b8e629c9be9805
cvVal <cvvaldez_00@hotmail.com>
2019-08-04 13:57:10 -0500

    Add mapper & update test

--- src/main/java/guru/microservices/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/microservices/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,9 @@
 package guru.microservices.msscbeerservice.web.controller;
 
+import guru.microservices.msscbeerservice.mappers.BeerMapper;
+import guru.microservices.msscbeerservice.repositories.BeerRepository;
 import guru.microservices.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,24 +11,39 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
+    private final BeerMapper beerMapper;
+    private final BeerRepository beerRepository;
+
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
 
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        return new ResponseEntity<>(beerMapper.beerToBeerDto(beerRepository.findById(beerId).get()), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDto beerDto) {
+
+        beerRepository.save(beerMapper.beerDtoToBeer(beerDto));
 
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto) {
+
+        beerRepository.findById(beerId).ifPresent(beer -> {
+            beer.setBeerName(beerDto.getBeerName());
+            beer.setBeerStyle(beerDto.getBeerStyle().name());
+            beer.setPrice(beerDto.getPrice());
+            beer.setUpc(beerDto.getUpc());
+
+            beerRepository.save(beer);
+        });
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
--- src/test/java/guru/microservices/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/microservices/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,21 +1,29 @@
 package guru.microservices.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.microservices.msscbeerservice.domain.Beer;
+import guru.microservices.msscbeerservice.repositories.BeerRepository;
 import guru.microservices.msscbeerservice.web.model.BeerDto;
 import guru.microservices.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "guru.microservices.msscbeerservice.mappers")
 class BeerControllerTest {
 
     @Autowired
@@ -24,8 +32,13 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerRepository beerRepository;
+
     @Test
     void getBeerById() throws Exception {
+
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
