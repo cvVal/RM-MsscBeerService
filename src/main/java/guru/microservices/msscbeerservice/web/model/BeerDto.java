8a0a33c8abd3aac51848244dcd4d24a71825824f
cvVal <cvvaldez_00@hotmail.com>
2019-08-18 14:49:56 -0500

    Adding pattern for last modified date

--- src/main/java/guru/microservices/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/microservices/msscbeerservice/web/model/BeerDto.java
@@ -36,11 +36,12 @@ public class BeerDto implements Serializable {
     private OffsetDateTime createdDate;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
-    @JsonSerialize(using = LocalDateSerializer.class)
+    /*@JsonSerialize(using = LocalDateSerializer.class)
     @JsonDeserialize(using = LocalDateDeserializer.class)
-    private LocalDate localDate;
+    private LocalDate localDate;*/
 
     @NotBlank
     @Size(min = 3, max = 100)
