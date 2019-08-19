048a917d35634e8e1f10787e8d60977aafc93598
cvVal <cvvaldez_00@hotmail.com>
2019-08-18 22:50:20 -0500

    Enable Loader to fix database data issue

--- src/main/java/guru/microservices/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/microservices/msscbeerservice/bootstrap/BeerLoader.java
@@ -2,58 +2,56 @@ package guru.microservices.msscbeerservice.bootstrap;
 
 import guru.microservices.msscbeerservice.domain.Beer;
 import guru.microservices.msscbeerservice.repositories.BeerRepository;
+import guru.microservices.msscbeerservice.web.model.BeerStyleEnum;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
-//@Component
+@Component
+@RequiredArgsConstructor
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "06312750024001";
     public static final String BEER_2_UPC = "06312750024002";
     public static final String BEER_3_UPC = "06312750024003";
-    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
 
     private final BeerRepository beerRepository;
 
-    public BeerLoader(BeerRepository beerRepository) {
-        this.beerRepository = beerRepository;
-    }
-
     @Override
     public void run(String... args) throws Exception {
-        loadBeerObjects();
+        if (beerRepository.count() == 0)
+            loadBeerObjects();
     }
 
     private void loadBeerObjects() {
 
-        if (beerRepository.count() == 0) {
+        beerRepository.save(Beer.builder()
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyleEnum.GOSE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .upc(BEER_1_UPC)
+                .price(new BigDecimal("23.99"))
+                .build());
 
-            beerRepository.save(Beer.builder()
-                    .beerName("Mango Bobs")
-                    .beerStyle("IPA")
-                    .quantityToBrew(200)
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("23.99"))
-                    .build());
+        beerRepository.save(Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .upc(BEER_2_UPC)
+                .price(new BigDecimal("11.99"))
+                .build());
 
-            beerRepository.save(Beer.builder()
-                    .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11.99"))
-                    .build());
-
-            beerRepository.save(Beer.builder()
-                    .beerName("Bohemia")
-                    .beerStyle("Dark")
-                    .quantityToBrew(200)
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("13.99"))
-                    .build());
-        }
+        beerRepository.save(Beer.builder()
+                .beerName("Bohemia")
+                .beerStyle(BeerStyleEnum.PORTER.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .upc(BEER_3_UPC)
+                .price(new BigDecimal("13.99"))
+                .build());
     }
 }
--- src/test/java/guru/microservices/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
+++ src/test/java/guru/microservices/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -1,13 +1,10 @@
 package guru.microservices.msscbeerservice.services.inventory;
 
-import guru.microservices.msscbeerservice.bootstrap.BeerLoader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @Disabled
 @SpringBootTest
@@ -24,7 +21,7 @@ class BeerInventoryServiceRestTemplateImplTest {
     @Test
     void getOnHandInventory() {
 
-        Integer qoh = beerInventoryService.getOnHandInventory(BeerLoader.BEER_1_UUID);
-        System.out.println(qoh);
+        //Integer qoh = beerInventoryService.getOnHandInventory(BeerLoader.BEER_1_UUID);
+        //System.out.println(qoh);
     }
 }
